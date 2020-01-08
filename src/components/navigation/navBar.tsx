ed8fa582dfaa26271857930a83cc3ffd64713173
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-01-08 13:35:57 +0200

    navbar remove unused things

--- src/components/navigation/navBar.tsx
+++ src/components/navigation/navBar.tsx
@@ -4,10 +4,7 @@ import { withStyles } from '@material-ui/styles'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu'
-
+import Button from '@material-ui/core/Button'
 
 const styles = {
   root: {
@@ -34,13 +31,10 @@ const NavBar = (props: any) => {
         <Button color="primary">
           <img className={classes.logo} src="static/bmc.png" alt="bmc" />
         </Button>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Enquire Now</Button>
         </Toolbar>
       </AppBar>
     </div>
