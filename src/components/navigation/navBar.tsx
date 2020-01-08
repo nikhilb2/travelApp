3d69fcbf025d445c356d2058afd9b85d0a346de9
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-01-08 12:39:40 +0200

    navigation bar edit

--- src/components/navigation/navBar.tsx
+++ src/components/navigation/navBar.tsx
@@ -1,15 +1,42 @@
 import React from 'react'
-//import theme from '../../theme'
+import theme from '../../theme'
 import { withStyles } from '@material-ui/styles'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'
+
 
 const styles = {
-
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.primary.main
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  }
 }
 
-const NavBar = () => {
+const NavBar = (props: any) => {
+  const { classes } = props
   return(
-    <div>
-    Coming soon
+    <div className={classes.root}>
+      <AppBar position="static" color={'inherit'}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
