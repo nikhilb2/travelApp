e951e2a95b6f45e766b0116910fe684651770cab
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-01-13 09:49:00 +0200

    travel card styling

--- src/components/travelCard/travelCard.tsx
+++ src/components/travelCard/travelCard.tsx
@@ -5,14 +5,30 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import theme from '../../theme'
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 150,
   },
   media: {
-    height: 200,
+    height: 120,
   },
+  heading: {
+    lineHeight: 1.2
+  },
+  price: {
+    backgroundColor: 'rgba(0,0,0, 0.5)',
+    color: theme.palette.primary.light,
+    width: 'fit-content',
+    padding: theme.spacing(.2)
+  },
+  miniDescription: {
+    marginTop: theme.spacing(1)
+  },
+  cardContent: {
+    height: 120
+  }
 });
 
 export default function MediaCard() {
@@ -23,15 +39,16 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/cat.jpg"
+          image="/static/phi.jpg"
           title="Contemplative Reptile"
+          children={<Typography className={classes.price}>₹50000</Typography>}
         />
-        <CardContent>
-          <Typography gutterBottom variant="subtitle1" >
-            Lizard
+        <CardContent className={classes.cardContent}>
+          <Typography className={classes.heading} variant="subtitle1" >
+            Luxury Phuket Phi phi
           </Typography>
-          <Typography variant="caption" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles
+          <Typography className={classes.miniDescription} variant="caption" color="textSecondary" component="p">
+            Experience 5 nights 4 days in luxurios resort in a peacefull resort in the jungles of phiphi island
           </Typography>
         </CardContent>
       </CardActionArea>
