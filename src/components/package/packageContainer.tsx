940a4d737de8f074faa80c3e004e626d1dfb0843
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-01-14 13:20:50 +0200

    package description

--- src/components/package/packageContainer.tsx
+++ src/components/package/packageContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 import ImageViewer from './imageViewer'
 import PackageDescription from './packageDescription'
 import theme from '../../theme'
@@ -8,28 +9,60 @@ import theme from '../../theme'
 const PackageContainer = (props: any) => {
   const { classes } = props
   return(
-    <Box className={classes.root}>
-      <Box className={classes.leftSide}>
-        <ImageViewer />
+    <Box>
+      <Box className={classes.imageAndDescriptionContainer}>
+        <Box className={classes.leftSide}>
+          <ImageViewer />
+        </Box>
+        <Box className={classes.rightSide}>
+          <PackageDescription />
+        </Box>
       </Box>
-      <Box className={classes.rightSide}>
-        <PackageDescription />
+      <Box boxShadow={4} className={classes.description}>
+        <Typography className={classes.detailsHeading} variant='h6'>
+          Details
+        </Typography>
+        <Typography className={classes.descriptionText}>
+          Phi phi island Phi phi islandPhi phi island  Phi phi islandPhiPhi phi islandPhi phi islandPhi phi island Phi phi island Phi phi islandPhi phi islandPhi phi island
+          Phi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi islandPhi phi island
+        </Typography>
       </Box>
     </Box>
   )
 }
 
 export default withStyles({
-  root: {
+  imageAndDescriptionContainer: {
     display: 'flex',
     flexDirection: 'row',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column'
+    },
     justifyContent: 'space-around',
     padding: theme.spacing(4)
   },
   leftSide: {
-    width: '40%'
+    width: '40%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%'
+    },
   },
   rightSide: {
-    width: '60%'
+    [theme.breakpoints.down('sm')]: {
+      width: '100%'
+    }
+  },
+  description: {
+    margin: theme.spacing(1),
+    width: '80%',
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  },
+  descriptionText: {
+    padding: theme.spacing(1)
+  },
+  detailsHeading: {
+  //  paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(1)
   }
 })(PackageContainer)
