6bc66374abbf5c4bc82e3f4b4f8d88d63f68edea
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-02-11 14:57:01 +0200

    image selector in add place

--- src/components/places/addNewPlace.tsx
+++ src/components/places/addNewPlace.tsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Box from '@material-ui/core/Box'
 import { fetchRequest } from '../../utils/request'
+import ImageSelector from '../createPackage/imageSelector'
 
 const AddNew = (props: any) => {
   const { setResults, selectedCountry, resetCountry } = props
 //  const [ keyword, setKeyword ] = useState("")
 //  const [ result, setResults ] = useState(Array())
   const [ keyword, setKeyword ] = useState("")
+  const [ selectedImages, selectImages ] = useState(Array())
+  const [ place, setPlace ] = useState("")
   const getCountries = async (k: string) => {
 
     const resultCountries = await fetchRequest(`get_country.php?keyword=${k}`, {
@@ -38,6 +41,22 @@ const AddNew = (props: any) => {
 
         }}
       />
+      <TextField
+        id="outlined-email-input"
+        label="Enter place name"
+        type="text"
+        name="text"
+        autoComplete="off"
+        value={place}
+        margin="normal"
+        variant="outlined"
+        onChange={ (e: any) => {
+        //  console.log(e.target.value)
+          setPlace(e.target.value)
+        }}
+      />
+      <ImageSelector selectImages={selectImages} selectedImages={selectedImages} />
+
     </Box>
   )
 }
