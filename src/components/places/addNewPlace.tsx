00bda181a2d8d3ab5ffa9f0fbecb82f7aa65c7e0
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-02-11 17:10:39 +0200

    add new place fetch

--- src/components/places/addNewPlace.tsx
+++ src/components/places/addNewPlace.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Box from '@material-ui/core/Box'
-import { fetchRequest } from '../../utils/request'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { fetchRequest, requestUploadImage } from '../../utils/request'
 import ImageSelector from '../createPackage/imageSelector'
 
 const AddNew = (props: any) => {
@@ -9,8 +11,34 @@ const AddNew = (props: any) => {
 //  const [ keyword, setKeyword ] = useState("")
 //  const [ result, setResults ] = useState(Array())
   const [ keyword, setKeyword ] = useState("")
+  const [ loader, setLoader ] = useState(false)
+  const [ message, setMessage ] = useState('')
   const [ selectedImages, selectImages ] = useState(Array())
   const [ place, setPlace ] = useState("")
+  const addNewPlace = async (data: any) => {
+    const result: any = await requestUploadImage(data.image)
+    if (!result.error) {
+      data.image = result.data.images[0]
+      const addPlace = await fetchRequest('insert_place.php', {
+        method: 'POST',
+        body: JSON.stringify(data)
+      }, true)
+
+      if (addPlace.message === 'success') {
+        setLoader(false)
+        selectImages(Array())
+        resetCountry()
+        setMessage(addPlace.message)
+      } else if (addPlace.error) {
+        setLoader(false)
+        setMessage(addPlace.error)
+      }
+      console.log(addPlace);
+      console.log(loader);
+    }
+  }
+
+
   const getCountries = async (k: string) => {
 
     const resultCountries = await fetchRequest(`get_country.php?keyword=${k}`, {
@@ -56,7 +84,13 @@ const AddNew = (props: any) => {
         }}
       />
       <ImageSelector selectImages={selectImages} selectedImages={selectedImages} />
+      <Button onClick={() => addNewPlace({
+        image: selectedImages,
+        country: selectedCountry.id,
+        place: place
 
+      })}>Add New Place</Button>
+      {loader ? <Typography>Loading...</Typography> : <Typography>{message}</Typography>}
     </Box>
   )
 }
