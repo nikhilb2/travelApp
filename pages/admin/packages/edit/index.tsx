f91463688af25eca8d0c3030830f986e6516d49e
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-02-12 21:14:49 +0200

    get package details from params in admin

--- pages/admin/packages/edit/index.tsx
+++ pages/admin/packages/edit/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import EditPackage from '../../../../src/pageContainers/editPackage';
 import { userDetails } from '../../../../hocs/auth-hoc'
-
+import fetch from 'isomorphic-unfetch'
+import { decoratedUrl } from '../../../../src/utils/request'
 
 const Edit = (props: any) => {
   return(
@@ -11,8 +12,18 @@ const Edit = (props: any) => {
   )
 }
 
-Edit.getInitialProps = async () => {
-  return {test: 'test'}
+Edit.getInitialProps = async ({ req, query }: any) => {
+  console.log(req);
+
+
+  const packageFetch =   query.id ? await fetch(
+  decoratedUrl(
+    query.id ? `get_package_by_id.php?packageid=${query.id}` : 'products'
+  )
+) : null
+
+const pack = packageFetch ? await packageFetch.json() : null
+  return {pack: pack ? pack.data : null}
 }
 
 
