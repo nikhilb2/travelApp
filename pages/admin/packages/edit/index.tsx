1112633dda1348138324a000a44bd9debbb6bce1
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-02-13 11:13:05 +0200

    indentation edit package page

--- pages/admin/packages/edit/index.tsx
+++ pages/admin/packages/edit/index.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import EditPackage from '../../../../src/pageContainers/editPackage';
-import { userDetails } from '../../../../hocs/auth-hoc'
-import fetch from 'isomorphic-unfetch'
-import { decoratedUrl } from '../../../../src/utils/request'
+import React from "react"
+import EditPackage from "../../../../src/pageContainers/editPackage"
+import { userDetails } from "../../../../hocs/auth-hoc"
+import fetch from "isomorphic-unfetch"
+import { decoratedUrl } from "../../../../src/utils/request"
 
 const Edit = (props: any) => {
-  return(
+  return (
     <div>
-      <EditPackage {...props}/>
+      <EditPackage {...props} />
     </div>
   )
 }
 
 Edit.getInitialProps = async ({ req, query }: any) => {
-  console.log(req);
+  console.log(req)
 
+  const packageFetch = query.id
+    ? await fetch(
+        decoratedUrl(
+          query.id ? `get_package_by_id.php?packageid=${query.id}` : "products"
+        )
+      )
+    : null
 
-  const packageFetch =   query.id ? await fetch(
-  decoratedUrl(
-    query.id ? `get_package_by_id.php?packageid=${query.id}` : 'products'
-  )
-) : null
-
-const pack = packageFetch ? await packageFetch.json() : null
-  return {pack: pack ? pack.data : null}
+  const pack = packageFetch ? await packageFetch.json() : null
+  return { pack: pack ? pack.data : null }
 }
-
 
 export default userDetails(Edit)
