b7e690309bfdeaf096dd75bfd341943db44eb8ec
nikhil bhatia <nikhilbhatia2004@gmail.com>
2020-02-15 15:20:34 +0200

    indentation for table.tsx

--- src/components/createPackage/editPackage/table.tsx
+++ src/components/createPackage/editPackage/table.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React from "react"
 import { withStyles } from "@material-ui/core/styles"
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
-import theme from '../../../theme'
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import theme from "../../../theme"
 
 const Table = (props: any) => {
-
   const { data, classes } = props
 
-  return(
+  return (
     <Box className={classes.root}>
       <div className={classes.tableHolder}>
-        <Typography className={classes.heading} style={{color: 'green'}}>Inclusions</Typography>
+        <Typography className={classes.heading} style={{ color: "green" }}>
+          Inclusions
+        </Typography>
         <div className={classes.content}>
-          {data.inclusions.split('#').map((row: string) => <div key={row}><TextField           InputProps={{
-            classes: {
-              input: classes.resize,
-            },
-          }} className={classes.contentText} value={row}/></div>)}
+          {data.inclusions.split("#").map((row: string) => (
+            <div key={row}>
+              <TextField
+                InputProps={{
+                  classes: {
+                    input: classes.resize
+                  }
+                }}
+                className={classes.contentText}
+                value={row}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className={classes.tableHolder}>
-        <Typography className={classes.heading} style={{color: 'red'}}>Exclusions</Typography>
+        <Typography className={classes.heading} style={{ color: "red" }}>
+          Exclusions
+        </Typography>
         <div className={classes.content}>
-          {data.exclusions.split('#').map((row: string) => <div key={row}><TextField           InputProps={{
-            classes: {
-              input: classes.resize,
-            },
-          }} className={classes.contentText} value={row}/></div>)}
+          {data.exclusions.split("#").map((row: string) => (
+            <div key={row}>
+              <TextField
+                InputProps={{
+                  classes: {
+                    input: classes.resize
+                  }
+                }}
+                className={classes.contentText}
+                value={row}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </Box>
@@ -37,51 +56,49 @@ const Table = (props: any) => {
 
 export default withStyles({
   root: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
       marginBottom: theme.spacing(1)
     },
     marginTop: theme.spacing(3),
-    width: '100%'
+    width: "100%"
   },
   heading: {
-  //  paddingRight: theme.spacing(1),
-  //  paddingLeft: theme.spacing(1)
+    //  paddingRight: theme.spacing(1),
+    //  paddingLeft: theme.spacing(1)
     borderBottom: `1px solid ${theme.palette.secondary.dark}`,
-    textAlign: 'center',
+    textAlign: "center",
     backgroundColor: theme.palette.secondary.main,
     ...theme.typography.caption,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
-
+    paddingRight: theme.spacing(1)
   },
   content: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start"
   },
   contentText: {
     borderBottom: `1px solid ${theme.palette.secondary.dark}`,
     ...theme.typography.caption,
-    width: '100%'
+    width: "100%"
   },
   resize: {
-    ...theme.typography.caption,
+    ...theme.typography.caption
   },
   tableHolder: {
     border: `1px solid ${theme.palette.secondary.dark}`,
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: '50%',
-    height: 'fit-content',
-    [theme.breakpoints.down('sm')]: {
+    width: "50%",
+    height: "fit-content",
+    [theme.breakpoints.down("sm")]: {
       marginBottom: theme.spacing(1),
-      width: '100%'
-    },
-
+      width: "100%"
+    }
   }
 })(Table)
